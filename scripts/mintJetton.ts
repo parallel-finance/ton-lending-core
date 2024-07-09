8b57888b6606abfefef6e42672b3536e9d37e903
alfred <lianghongbuaa@gmail.com>
2024-07-09 11:27:53 +0800

    add scripts

---
+++ scripts/mintJetton.ts
@@ -0,0 +1,30 @@
+import { Address, toNano } from '@ton/core';
+import { NetworkProvider, sleep } from '@ton/blueprint';
+import {SampleJetton} from "../build/SampleJetton/tact_SampleJetton";
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const userAddress = Address.parse(provider.sender().address?.toString() || '');
+    const ui= provider.ui();
+
+    const address = Address.parse('EQBgTTj_q3OVw9a8qVukPpN0IChMH-ZcL7Vj-zXlRF6Llc_3');
+
+    if (!(await provider.isContractDeployed(address))) {
+        ui.write(`Error: Contract at address ${address} is not deployed!`);
+        return;
+    }
+
+    const sampleJetton = provider.open(SampleJetton.fromAddress(address));
+
+    await sampleJetton.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Mint',
+            receiver: userAddress,
+            amount: 100000000000000n,
+        }
+    );
+    console.log(`Minted 100000000000000 SAM to ${userAddress.toString()}`);
+}
