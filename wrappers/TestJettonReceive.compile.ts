22a91a019536c868a14823ebc59769e3592d6f35
jfzhou5 <1241330802@qq.com>
2024-07-10 19:26:16 +0800

    add test for TestJettonReceive

--- wrappers/TestJettonReceive.compile.ts
+++ wrappers/TestJettonReceive.compile.ts
@@ -3,4 +3,7 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/misc/TestJettonReceive.tact',
+    options: {
+        debug: true,
+    },
 };
