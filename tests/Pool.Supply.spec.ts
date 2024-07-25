c7c52a7695f820216556927637680936604cb40d
alfred <lianghongbuaa@gmail.com>
2024-07-25 17:33:06 +0800

    add ton value calculation

--- tests/Pool.Supply.spec.ts
+++ tests/Pool.Supply.spec.ts
@@ -1,23 +1,16 @@
 import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { address, beginCell, Cell, toNano } from '@ton/core';
-import {
-    ATokenDTokenContents,
-    Pool,
-    ReserveConfiguration,
-    ReserveInterestRateStrategy,
-    UpdatePosition,
-    UpdatePositionBounce,
-} from '../wrappers/Pool';
+import { beginCell, Cell, toNano } from '@ton/core';
+import { Pool, UpdatePositionBounce } from '../wrappers/Pool';
 import '@ton/test-utils';
 import { SampleJetton } from '../build/SampleJetton/tact_SampleJetton';
-import { buildOnchainMetadata } from '../scripts/utils';
 import { JettonDefaultWallet } from '../build/SampleJetton/tact_JettonDefaultWallet';
 import { UserAccount } from '../build/Pool/tact_UserAccount';
 import { ATokenDefaultWallet } from '../build/AToken/tact_ATokenDefaultWallet';
 import { AToken } from '../wrappers/AToken';
 import { sumTransactionsFee } from '../jest.setup';
-import { RERUN_ACTION_MINT, RERUN_ACTION_UPDATE_POSITION } from '../helpers/constant';
+import { RERUN_ACTION_UPDATE_POSITION } from '../helpers/constant';
 import { parsePoolBounceMessage } from '../helpers/pool';
+import { addReserve, deployJetton, deployPool, mintJetton } from './utils';
 
 describe('Pool Supply', () => {
     let blockchain: Blockchain;
@@ -27,61 +20,14 @@ describe('Pool Supply', () => {
     let sampleJetton: SandboxContract<SampleJetton>;
     let aToken: SandboxContract<AToken>;
 
-    const reserveAddress = address('UQAEJ7U1iaC1TzcFel5lc2-JaEm8I0k5Krui3fzz3_GeANWV');
-
-    const reserveConfiguration: ReserveConfiguration = {
-        $$type: 'ReserveConfiguration',
-        poolWalletAddress: reserveAddress,
-        aTokenAddress: reserveAddress,
-        dTokenAddress: reserveAddress,
-        ltv: 6000n,
-        liquidationThreshold: 7500n,
-        liquidationBonus: 10500n,
-        reserveFactor: 1000n,
-        liquidationProtocolFee: 500n,
-        isActive: true,
-        isFrozen: false,
-        borrowingEnabled: true,
-        supplyCap: 1000000n,
-        borrowCap: 1000000n,
-    };
-
-    const reserveInterestRateStrategy: ReserveInterestRateStrategy = {
-        $$type: 'ReserveInterestRateStrategy',
-        optimalUsageRatio: BigInt(0.9 * 10 ** 27),
-        maxUsageRatio: BigInt(10 ** 27) - BigInt(0.9 * 10 ** 27),
-        baseBorrowRate: 0n,
-        slope1: BigInt(0.04 * 10 ** 27),
-        slope2: BigInt(0.6 * 10 ** 27),
-    };
-
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
         pool = blockchain.openContract(await Pool.fromInit());
-
         deployer = await blockchain.treasury('deployer');
-        secondUser = (await blockchain.createWallets(2))[1];
+        secondUser = await blockchain.treasury('secondUser');
+        const sender = deployer.getSender();
 
-        // deploy pool
-        const deployResult = await pool.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            },
-        );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: pool.address,
-            deploy: true,
-            success: true,
-        });
-
+        await deployPool(pool, deployer);
         // deploy test jetton
         const jettonParams = {
             name: 'SampleJetton',
@@ -90,78 +36,12 @@ describe('Pool Supply', () => {
             image: 'https://ipfs.io/ipfs/bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg',
             symbol: 'SAM',
         };
-        let max_supply = toNano(1000000n); // 🔴 Set the specific total supply in nano
-        let content = buildOnchainMetadata(jettonParams);
+        sampleJetton = await deployJetton(blockchain, deployer, jettonParams);
+        await mintJetton(sampleJetton, sender, deployer.address, toNano(100n));
 
-        const aTokenJettonParams = {
-            name: 'SampleJetton AToken',
-            description: 'Sample Jetton aToken',
-            decimals: '9',
-            image: 'https://ipfs.io/ipfs/bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg',
-            symbol: 'aSAM',
-        };
-        let aTokenContent = buildOnchainMetadata(aTokenJettonParams);
-        const contents: ATokenDTokenContents = {
-            $$type: 'ATokenDTokenContents',
-            aTokenContent,
-            dTokenContent: Cell.EMPTY, // TODO
-        };
-
-        sampleJetton = blockchain.openContract(await SampleJetton.fromInit(deployer.address, content, max_supply));
-
-        await sampleJetton.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            },
-        );
-
-        // add reserve
         const poolWalletAddress = await sampleJetton.getGetWalletAddress(pool.address);
-        await pool.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.2'),
-            },
-            {
-                $$type: 'AddReserve',
-                reserveAddress: sampleJetton.address,
-                reserveConfiguration: {
-                    ...reserveConfiguration,
-                    poolWalletAddress,
-                },
-                contents,
-                reserveInterestRateStrategy,
-            },
-        );
-
-        const calculateATokenAddress = await pool.getCalculateATokenAddress(
-            contents.aTokenContent,
-            sampleJetton.address,
-        );
-
-        aToken = blockchain.openContract(AToken.fromAddress(calculateATokenAddress));
-        expect((await aToken.getOwner()).toString()).toEqual(pool.address.toString());
-        expect((await aToken.getGetPoolData()).pool.toString()).toEqual(pool.address.toString());
-        expect((await aToken.getGetPoolData()).asset.toString()).toEqual(sampleJetton.address.toString());
-
-        // mint test jetton to deployer
-        await sampleJetton.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Mint',
-                queryId: 0n,
-                amount: 100000000000n,
-                receiver: deployer.address,
-            },
-        );
+        const { aTokenAddress } = await addReserve(pool, deployer, sampleJetton.address, poolWalletAddress);
+        aToken = blockchain.openContract(await AToken.fromAddress(aTokenAddress));
     });
 
     const supplyJetton = async (amount: bigint) => {
@@ -228,9 +108,48 @@ describe('Pool Supply', () => {
     };
 
     describe('handle supply', () => {
-        it('should handle supply successfully', async () => {
+        it('should handle jetton supply successfully', async () => {
             const amount = toNano(100n);
             await supplyJetton(amount);
+        });
+
+        it('should handle ton supply successfully', async () => {
+            await addReserve(pool, deployer, pool.address, pool.address);
+            const userAccountContract = blockchain.openContract(await UserAccount.fromInit(pool.address, deployer.address));
+            const userAccountAddress = userAccountContract.address;
+
+            const amount = toNano(100);
+            const result = await pool.send(
+                deployer.getSender(),
+                {
+                    value: toNano('100.25'),
+                },
+                {
+                    $$type: 'SupplyTon',
+                    amount,
+                },
+            );
+
+            // UpdatePosition
+            expect(result.transactions).toHaveTransaction({
+                from: pool.address,
+                to: userAccountAddress,
+                success: true,
+            });
+
+            // UserPositionUpdated
+            expect(result.transactions).toHaveTransaction({
+                from: userAccountAddress,
+                to: pool.address,
+                success: true,
+            });
+
+            // check user account
+            const accountData = await userAccountContract.getAccount();
+            expect(accountData.positionsLength).toEqual(1n);
+            expect(accountData.positions?.get(0n)!!.equals(pool.address)).toBeTruthy();
+            expect(accountData.positionsDetail?.get(pool.address)!!.supply).toEqual(amount);
+            expect(accountData.positionsDetail?.get(pool.address)!!.asCollateral).toBeTruthy();
         });
 
         it('transfer AToken', async () => {
@@ -488,11 +407,6 @@ describe('Pool Supply', () => {
             );
             const forward_payload: Cell = beginCell().storeUint(0x55b591ba, 32).endCell();
 
-            const userAccountContract = blockchain.openContract(
-                await UserAccount.fromInit(pool.address, deployer.address),
-            );
-            const userAccountAddress = userAccountContract.address;
-
             const result = await deployerJettonDefaultWallet.send(
                 deployer.getSender(),
                 {
@@ -509,8 +423,6 @@ describe('Pool Supply', () => {
                     forward_payload: forward_payload,
                 },
             );
-            printTransactionFees(result.transactions);
-
             // TokenTransferInternal
             expect(result.transactions).toHaveTransaction({
                 from: deployerWalletAddress,
--- tests/Pool.Withdraw.spec.ts
+++ tests/Pool.Withdraw.spec.ts
@@ -1,100 +1,33 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { address, beginCell, Cell, fromNano, toNano } from '@ton/core';
-import { ATokenDTokenContents, Pool, ReserveConfiguration, ReserveInterestRateStrategy } from '../wrappers/Pool';
+import { fromNano, toNano } from '@ton/core';
+import { Pool } from '../wrappers/Pool';
 import '@ton/test-utils';
 import { SampleJetton } from '../build/SampleJetton/tact_SampleJetton';
-import { buildOnchainMetadata } from '../scripts/utils';
 import { JettonDefaultWallet } from '../build/SampleJetton/tact_JettonDefaultWallet';
 import { UserAccount } from '../build/Pool/tact_UserAccount';
-import { DTokenDefaultWallet } from '../build/DToken/tact_DTokenDefaultWallet';
 import { AToken } from '../wrappers/AToken';
 import { DToken } from '../wrappers/DToken';
-import { PERCENTAGE_FACTOR, RERUN_ACTION_TOKEN_BURN, RERUN_ACTION_UPDATE_POSITION } from '../helpers/constant';
+import { PERCENTAGE_FACTOR } from '../helpers/constant';
 import { ATokenDefaultWallet } from '../build/AToken/tact_ATokenDefaultWallet';
 import { sleep } from '@ton/blueprint';
 import { sumTransactionsFee } from '../jest.setup';
+import { addReserve, deployJetton, deployPool, mintJetton, reserveConfiguration, supplyJetton } from './utils';
 
 describe('Pool Withdraw', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let secondUser: SandboxContract<TreasuryContract>;
     let pool: SandboxContract<Pool>;
     let sampleJetton: SandboxContract<SampleJetton>;
     let aToken: SandboxContract<AToken>;
     let dToken: SandboxContract<DToken>;
-    let contents: ATokenDTokenContents;
     let deployerJettonDefaultWallet: SandboxContract<JettonDefaultWallet>;
     let deployerATokenDefaultWallet: SandboxContract<ATokenDefaultWallet>;
-    let deployerDTokenDefaultWallet: SandboxContract<DTokenDefaultWallet>;
     let poolWallet: SandboxContract<JettonDefaultWallet>;
-
-    let rst;
-
-    const reserveAddress = address('UQAEJ7U1iaC1TzcFel5lc2-JaEm8I0k5Krui3fzz3_GeANWV');
-
-    const reserveConfiguration: ReserveConfiguration = {
-        $$type: 'ReserveConfiguration',
-        poolWalletAddress: reserveAddress,
-        aTokenAddress: reserveAddress,
-        dTokenAddress: reserveAddress,
-        ltv: 6000n,
-        liquidationThreshold: 7500n,
-        liquidationBonus: 10500n,
-        reserveFactor: 1000n,
-        liquidationProtocolFee: 500n,
-        isActive: true,
-        isFrozen: false,
-        borrowingEnabled: true,
-        supplyCap: 1000000n,
-        borrowCap: 1000000n,
-    };
-
-    const reserveInterestRateStrategy: ReserveInterestRateStrategy = {
-        $$type: 'ReserveInterestRateStrategy',
-        optimalUsageRatio: BigInt(0.9 * 10 ** 27),
-        maxUsageRatio: BigInt(10 ** 27) - BigInt(0.9 * 10 ** 27),
-        baseBorrowRate: 0n,
-        slope1: BigInt(0.04 * 10 ** 27),
-        slope2: BigInt(0.6 * 10 ** 27),
-    };
 
     const supplyAmount = toNano(100n);
 
-    const supplyFromDeployer = async (amount: bigint) => {
-        // transfer jetton to pool
-        const deployerWalletAddress = await sampleJetton.getGetWalletAddress(deployer.address);
-        deployerJettonDefaultWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(deployerWalletAddress));
-        const forward_payload: Cell = beginCell().storeUint(0x55b591ba, 32).endCell();
-
-        const userAccountContract = blockchain.openContract(await UserAccount.fromInit(pool.address, deployer.address));
-
-        await deployerJettonDefaultWallet.send(
-            deployer.getSender(),
-            {
-                value: toNano('3'),
-            },
-            {
-                $$type: 'TokenTransfer',
-                queryId: 0n,
-                amount: amount,
-                destination: pool.address,
-                response_destination: deployerWalletAddress,
-                custom_payload: null,
-                forward_ton_amount: toNano('2'),
-                forward_payload: forward_payload,
-            },
-        );
-        // check user account
-        const accountData = await userAccountContract.getAccount();
-        expect(accountData.positionsLength).toEqual(1n);
-        expect(accountData.positions?.get(0n)!!.equals(sampleJetton.address)).toBeTruthy();
-        expect(accountData.positionsDetail?.get(sampleJetton.address)!!.supply).toEqual(amount);
-        expect(accountData.positionsDetail?.get(sampleJetton.address)!!.asCollateral).toBeTruthy();
-    };
-
     const borrowFromDeployer = async (amount: bigint) => {
         const userAccountAddress = await UserAccount.fromInit(pool.address, deployer.address);
-        const borrowAmount = amount;
 
         let result = await pool.send(
             deployer.getSender(),
@@ -104,7 +37,7 @@ describe('Pool Withdraw', () => {
             {
                 $$type: 'BorrowToken',
                 tokenAddress: sampleJetton.address,
-                amount: borrowAmount,
+                amount: amount,
             },
         );
 
@@ -148,31 +81,11 @@ describe('Pool Withdraw', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
         pool = blockchain.openContract(await Pool.fromInit());
-
         deployer = await blockchain.treasury('deployer');
-        secondUser = (await blockchain.createWallets(2))[1];
+        const sender = deployer.getSender();
 
-        // deploy pool
-        const deployResult = await pool.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            },
-        );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: pool.address,
-            deploy: true,
-            success: true,
-        });
-
+        await deployPool(pool, deployer);
         // deploy test jetton
         const jettonParams = {
             name: 'SampleJetton',
@@ -181,128 +94,30 @@ describe('Pool Withdraw', () => {
             image: 'https://ipfs.io/ipfs/bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg',
             symbol: 'SAM',
         };
-        let max_supply = toNano(1000000n);
-        let content = buildOnchainMetadata(jettonParams);
+        sampleJetton = await deployJetton(blockchain, deployer, jettonParams);
+        await mintJetton(sampleJetton, sender, deployer.address, toNano(100n));
 
-        const aTokenJettonParams = {
-            name: 'SampleJetton AToken',
-            description: 'Sample Jetton aToken',
-            decimals: '9',
-            image: 'https://ipfs.io/ipfs/bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg',
-            symbol: 'aSAM',
-        };
-        let aTokenContent = buildOnchainMetadata(aTokenJettonParams);
-
-        const dTokenJettonParams = {
-            name: 'SampleJetton DToken',
-            description: 'Sample Jetton dToken',
-            decimals: '9',
-            image: 'https://ipfs.io/ipfs/bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg',
-            symbol: 'dSAM',
-        };
-        let dTokenContent = buildOnchainMetadata(dTokenJettonParams);
-        contents = {
-            $$type: 'ATokenDTokenContents',
-            aTokenContent,
-            dTokenContent,
-        };
-
-        sampleJetton = blockchain.openContract(await SampleJetton.fromInit(deployer.address, content, max_supply));
-
-        await sampleJetton.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            },
-        );
-
-        // add reserve
         const poolWalletAddress = await sampleJetton.getGetWalletAddress(pool.address);
-        poolWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(poolWalletAddress));
-        await pool.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.2'),
-            },
-            {
-                $$type: 'AddReserve',
-                reserveAddress: sampleJetton.address,
-                reserveConfiguration: {
-                    ...reserveConfiguration,
-                    poolWalletAddress,
-                },
-                contents,
-                reserveInterestRateStrategy,
-            },
-        );
-
-        rst = await pool.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.2'),
-            },
-            {
-                $$type: 'SetMockOraclePrice',
-                asset: sampleJetton.address,
-                price: toNano('1'),
-            },
-        );
-
-        // SetMockOraclePrice
-        expect(rst.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: pool.address,
-            success: true,
-        });
-
-        const calculateATokenAddress = await pool.getCalculateATokenAddress(
-            contents.aTokenContent,
+        const { aTokenAddress, dTokenAddress } = await addReserve(
+            pool,
+            deployer,
             sampleJetton.address,
+            poolWalletAddress,
         );
 
-        const calculateDTokenAddress = await pool.getCalculateDTokenAddress(
-            contents.dTokenContent,
-            sampleJetton.address,
-        );
-        console.log('calculateDTokenAddress', calculateDTokenAddress.toString());
+        aToken = blockchain.openContract(await AToken.fromAddress(aTokenAddress));
+        dToken = blockchain.openContract(DToken.fromAddress(dTokenAddress));
 
-        aToken = blockchain.openContract(AToken.fromAddress(calculateATokenAddress));
-        dToken = blockchain.openContract(DToken.fromAddress(calculateDTokenAddress));
+        const deployerWalletAddress = await sampleJetton.getGetWalletAddress(deployer.address!!);
+        deployerJettonDefaultWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(deployerWalletAddress));
+
         deployerATokenDefaultWallet = blockchain.openContract(
             ATokenDefaultWallet.fromAddress(await aToken.getGetWalletAddress(deployer.address)),
         );
-        deployerDTokenDefaultWallet = blockchain.openContract(
-            DTokenDefaultWallet.fromAddress(await dToken.getGetWalletAddress(deployer.address)),
-        );
-
-        expect((await aToken.getOwner()).toString()).toEqual(pool.address.toString());
-        expect((await aToken.getGetPoolData()).pool.toString()).toEqual(pool.address.toString());
-        expect((await aToken.getGetPoolData()).asset.toString()).toEqual(sampleJetton.address.toString());
-
-        expect((await dToken.getOwner()).toString()).toEqual(pool.address.toString());
-        expect((await dToken.getGetPoolData()).pool.toString()).toEqual(pool.address.toString());
-        expect((await dToken.getGetPoolData()).asset.toString()).toEqual(sampleJetton.address.toString());
-
-        // mint test jetton to deployer
-        await sampleJetton.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Mint',
-                queryId: 0n,
-                amount: 100000000000n,
-                receiver: deployer.address,
-            },
-        );
 
         // supply
-        await supplyFromDeployer(supplyAmount);
+        await supplyJetton(deployerJettonDefaultWallet, deployer, pool.address, supplyAmount);
+        poolWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(poolWalletAddress));
     });
 
     it('withdraw successfully with no debt', async () => {
